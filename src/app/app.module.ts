b08ddae6928a2a66d7fc60aa1b30fecb868edf5b
rupertoss <rakoski.rupertoss@gmail.com>
2018-06-05 11:38:04 +0200

    Update App Module
    Updating view and adding HttpClientModule to imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ArticleListComponent } from './article-list/article-list.component';
     ArticleListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
