813af4533d927441821c8ba4c553e8ab6a8c4d8d
rupertoss <rakoski.rupertoss@gmail.com>
2018-06-05 10:19:06 +0200

    Create Article model

---
+++ src/app/shared/article.ts
@@ -0,0 +1,10 @@
+export class Article {
+  
+  constructor(public author: string,
+              public title: string,
+              public description: string,
+              public date: string,
+              public sourceName: string,
+              public imageUrl: string) {
+  }
+}
